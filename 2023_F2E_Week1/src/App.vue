bceb4b5a8eb7aad842c1227e36dd16d1c83521cd
raylanraylan <startup9043672@gmail.com>
2023-11-09 17:41:40 +0800

    phone header&cover/news background

--- 2023_F2E_Week1/src/App.vue
+++ 2023_F2E_Week1/src/App.vue
@@ -1,47 +1,76 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <nav class="navbar bg-body-tertiary fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Offcanvas navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li>
+                    <hr class="dropdown-divider">
+                  </li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="d-flex mt-3" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </nav>
   </header>
 
   <main>
-    <TheWelcome />
+    <section class="cover"></section>
+    <section class="news"></section>
+    <section class="policy"></section>
+    <section class="donate"></section>
+    <section class="public_feedback"></section>
   </main>
+
+  <footer>
+
+  </footer>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style lang="scss" scoped>
+.offcanvas{
+  transition:none;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.cover,.news,.donate,.public_feedback{
+  margin: 0 5px 0;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.cover{
+  padding: 30px 20px 20px;
+  background-color: $light5;
 }
+
 </style>
--- 2023_F2E_Week1/src/main.js
+++ 2023_F2E_Week1/src/main.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
+import './assets/base.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+
+import "bootstrap/scss/bootstrap.scss"
+import "bootstrap/js/index.umd"
 
 createApp(App).mount('#app')
